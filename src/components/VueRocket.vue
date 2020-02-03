ab15d09744599f19a58eba49e1aa6a1994b377a3
anton.milukov <anton.milukov@gmail.com>
2020-02-03 16:38:30 +0400

    * upd

--- src/components/VueRocket.vue
+++ src/components/VueRocket.vue
@@ -1,37 +1,36 @@
 <template>
-  <div class="hello">
+  <div>
     <h1>{{ intro }}</h1>
+
+    <p>
+        <input id="username" type="text" v-model="username"> Username
+    </p>
+
+    <p>
+        <input id="password" type="password" v-model="password"> Password
+    </p>
+
+    <div v-if="roomId">
+      <h3>{{ roomId }}</h3>
+      <ul>
+        <li v-for="(message, $index) in messages" :key="$index">{{ message }}</li>
+      </ul>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'VueRocket',
-  props: {
-    msg: String
-  },
   data: ()=>{
     return {
-      intro: "RocketVue component by Anton Milukov"
+      intro: "RocketVue component by Anton Milukov",
+      username: '',
+      password: '',
+      messages: ['123', '456', '789'],
+      roomId: '1',
     };
-  }
+  },
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
