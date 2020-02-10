28ea9187298dab31321451343862b675342af060
anton.milukov <anton.milukov@gmail.com>
2020-02-10 13:09:11 +0400

    * auth

--- src/components/VueRocket.vue
+++ src/components/VueRocket.vue
@@ -1,22 +1,40 @@
+<!--@todo: auth, sync with interval, -->
 <template>
   <div>
     <h1>{{ intro }}</h1>
 
     <div>
       <p>
-          <input id="username" type="text" v-model="username"> Username
+          <input id="username" type="text" v-model="username" autocomplete="username" name="username"> Username
       </p>
 
       <p>
-          <input id="password" type="password" v-model="password"> Password
+          <input id="password" type="password" v-model="password" autocomplete="password" name="password"> Password
       </p>
       <p>
-        <input type="button" v-on:click="auth" value="auth">
+        <input type="button" v-on:click="authAndGetRooms" value="auth">
       </p>
     </div>
 
-    <h3>Write a message</h3>
-    <div>
+    <div v-if="connect.userId && connect.authToken">
+      <h3>Available Rooms</h3>
+      <ul>
+        <li v-for="(room, $index) in rooms" :key="$index">{{ room }}</li>
+      </ul>
+    </div>
+
+    <div v-if="rooms.length > 0">
+      <h3>Connect to room</h3>
+      <p>
+        <input id="roomId" type="text" v-model="connect.roomId"> Id
+      </p>
+      <p>
+        <input type="button" v-on:click="connectToRoom" value="connect">
+      </p>
+    </div>
+
+    <div v-if="connect.roomId">
+      <h3>Write a message</h3>
       <p>
         <input id="new_message" type="text" v-model="newMessage"> New message
       </p>
@@ -26,9 +44,8 @@
       </p>
     </div>
 
-
-    <div v-if="roomId">
-      <h3>Messages in room {{ roomId}}</h3>
+    <div v-if="connect.roomId">
+      <h3>Messages in room {{ connect.roomId}}</h3>
       <ul>
         <li v-for="(message, $index) in messages" :key="$index">{{ message }}</li>
       </ul>
@@ -44,18 +61,24 @@ export default {
   name: 'VueRocket',
   data: ()=>{
     return {
+      connect: {
+        authToken: null,
+        userId: null,
+        roomId: null
+      },
+      rooms: [],
       intro: "RocketVue component",
-      websocketUrl: "wss://demo.rocket.chat/websocket",
+      websocketUrl: "wss://open.rocket.chat/websocket",
       username: '',
       password: '',
       messages: [],
       newMessage: '',
-      roomId: 'Drjw54ftqGa4antMW',
-      authState: null
+      authState: 0
     };
   },
   mounted() {
     api = new RealTimeAPI(this.websocketUrl);
+    api.connectToServer();
     api.keepAlive().subscribe();
     api.onMessage(message =>{
       this.messages.push(message);
@@ -63,8 +86,33 @@ export default {
   },
   methods: {
     auth(){
-      this.authState = api.login(this.username, this.password);
+      return api.login(this.username, this.password);
     },
+    getRooms(){
+      //
+    },
+    authAndGetRooms(){
+      this.auth().subscribe(
+        res => {
+          if (res.msg !== "result") return;
+
+          // eslint-disable-next-line no-console
+          console.log(res);
+          if ('result' in res){
+            if ('token' in res.result){
+              this.connect.authToken = res.result.token;
+            }
+
+            if ('id' in res.result){
+              this.connect.userId = res.result.id;
+            }
+
+
+          }
+        }
+      );
+    },
+
     sendMessage(){
       let id = new Date().getDate();
       api.sendMessage({
@@ -74,9 +122,21 @@ export default {
         "params": [
           {
             "_id": id,
-            "rid": this.roomId,
+            "rid": this.connect.roomId,
             "msg": this.newMessage
           }
+        ]
+      });
+      this.newMessage = '';
+    },
+
+    connectToRoom(){
+      api.sendMessage({
+        "msg": "method",
+        "method": "openRoom",
+        "id": this.connect.roomId,
+        "params": [
+          "roomId"
         ]
       });
     }
